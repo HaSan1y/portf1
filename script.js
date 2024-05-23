b03cd8b87c18e6afdc63ab1185cd1b849676e118
Hasan Yildirim <endplay_xp@hotmail.com>
2024-05-23 03:07:32 +0200

    nw

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
 function togglemenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+   const menu = document.querySelector(".menu-links");
+   const icon = document.querySelector(".hamburger-icon");
+   menu.classList.toggle("open");
+   icon.classList.toggle("open");
 }
 //cookielaw
 const cookieBox = document.querySelector(".wrapper");
 const buttons = document.querySelectorAll(".button");
 
 const executeCodes = () => {
-  if (document.cookie.includes("profile")) return;
+   if (document.cookie.includes("profile")) return;
 
-  cookieBox.classList.add("show");
+   cookieBox.classList.add("show");
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      cookieBox.classList.remove("show");
-      if (button.id == "acceptBtn") {
-        //cookies: 1 month 60=1min 60=1hr 30=30days; samesitestrict=only within the orign-page;
-        document.cookie =
-          "cookieBy= profile; SameSite=Strict; max-age= " + 60 * 60 * 24 * 30;
-      }
-    });
-  });
+   buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+         cookieBox.classList.remove("show");
+         if (button.id == "acceptBtn") {
+            //cookies: 1 month 60=1min 60=1hr 30=30days; samesitestrict=only within the orign-page;
+            document.cookie = "cookieBy= profile; SameSite=Strict; max-age= " + 60 * 60 * 24 * 30;
+         }
+      });
+   });
 };
 
 window.addEventListener("load", executeCodes);
 
 
 function xx(){
- let y=document.documentElement;
- if(y.classList=='dark'){y.classList.replace('dark','white');}else{
-  y.classList.replace('white','dark');
- }
-  
+ document.documentElement.classList.toggle('white');
+//  if(y.classList=='dark'){y.classList.replace('dark','white');}else{
+//   y.classList.replace('white','dark');
+//  }
+// if (y.classList.contains("dark")  
 }
-
-
-
-
-
-
-
